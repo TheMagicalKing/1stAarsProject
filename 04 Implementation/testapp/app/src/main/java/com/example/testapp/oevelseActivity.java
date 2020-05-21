9dec71d0ab78ef390c1722630bbb18bc850a185b
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-05-21 10:11:18 +0200

    Minor fix

--- 04 Implementation/testapp/app/src/main/java/com/example/testapp/oevelseActivity.java
+++ 04 Implementation/testapp/app/src/main/java/com/example/testapp/oevelseActivity.java
@@ -30,7 +30,7 @@ public class oevelseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_oevelse);
         editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
