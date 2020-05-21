31082614870af19f8832b7593b89170d9c9fa210
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-05-21 10:26:19 +0200

    fik øvelse knappen til at fungere

--- 04 Implementation/testapp/app/src/main/java/com/example/testapp/forsideActivity.java
+++ 04 Implementation/testapp/app/src/main/java/com/example/testapp/forsideActivity.java
@@ -17,23 +17,29 @@ public class forsideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forside);
 
         final Button chatButton = findViewById(R.id.chatButton);
+        final Button oevelseButon = findViewById(R.id.oevelserButton);
         chatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openChat();
             }
         });
-
+        oevelseButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openOevelse();
+            }
+        });
     }
 
-    private void openChat(){
+    private void openChat() {
 
         Intent intent = new Intent(this, klientChat.class);
         startActivity(intent);
 
     }
 
-    private void openOevelse(){
+    private void openOevelse() {
 
         Intent intent = new Intent(this, oevelseActivity.class);
         startActivity(intent);
