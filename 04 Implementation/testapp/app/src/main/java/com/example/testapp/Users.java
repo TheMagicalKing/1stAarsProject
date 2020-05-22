c195351cf16428a1f40641c71024327fbc4bf588
sune <sune1309@edu.easj.dk>
2020-05-22 11:55:20 +0200

    God is dead for I have killed him

--- 04 Implementation/testapp/app/src/main/java/com/example/testapp/Users.java
+++ 04 Implementation/testapp/app/src/main/java/com/example/testapp/Users.java
@@ -1,13 +1,27 @@
 package com.example.testapp;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Users extends AppCompatActivity {
     ListView userList;
@@ -22,6 +36,56 @@ public class Users extends AppCompatActivity {
         setContentView(R.layout.activity_users);
 
         userList = (ListView)findViewById(R.id.usersList);
+        noUsersText = (TextView)findViewById(R.id.noUsersText);
+
+        pd = new ProgressDialog(Users.this);
+        pd.setMessage("loading ..");
+        pd.show();
+
+        String url = "https://console.firebase.google.com/project/chat-test-6a7f9/authentication/users";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                doOnSuccess(s);
+            },new Response.ErrorListener(){
+                @Override
+                        public void onErrorResponse(VolleyError volleyError){
+                    System.out.println("" + volleyError);
+                }
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(Users.this);
+        requestQueue.add(request);
+
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                UserDetails.chatWith = al.get(position);
+                startActivity(new Intent(Users.this, klientChat.class));
+            }
+        });
+    }
+
+    public void  doOnSuccess(String s){
+        try {
+            JSONObject object = new JSONObject(s);
+            Iterator i = object.keys();
+            String key = "";
+
+            while (i.hasNext()){
+                key = i.next().toString();
+
+                if (!key.equals(UserDetails.username)){
+                    al.add(key);
+                }
+
+                totalUser++;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
