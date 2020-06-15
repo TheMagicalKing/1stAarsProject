74276c8dd7e078a4fcffc7ba5dd966d1c5a91e51
sune <sune1309@edu.easj.dk>
2020-06-15 09:17:47 +0200

    GRASPING it firmly pt2

--- 04 Implementation/testapp/app/src/main/java/com/example/testapp/persistens/NewUserDBController.java
+++ 04 Implementation/testapp/app/src/main/java/com/example/testapp/persistens/NewUserDBController.java
@@ -1,5 +1,17 @@
 package com.example.testapp.persistens;
 
-public class NewUserDBController {
+//This conntroller handels connections to userDB
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class NewUserDBController extends AppCompatActivity {
+
+FirebaseAuth firebaseAuth;
+
+
+
+
 
 }
