9425eeff5a7e19843cf8f2be64373bdd69960c4a
sune <sune1309@edu.easj.dk>
2020-06-17 10:41:07 +0200

    Refactoring

--- 04 Implementation/testapp/app/src/main/java/com/example/testapp/UI/BookingActivity.java
+++ 04 Implementation/testapp/app/src/main/java/com/example/testapp/UI/BookingActivity.java
@@ -2,30 +2,32 @@ package com.example.testapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.CookieManager;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
-
-import com.example.testapp.Logic.OevelseListActivity;
 import com.example.testapp.R;
+
+// Denne fil kontrollere vores booking system.
 
 public class BookingActivity extends AppCompatActivity {
     WebView bookingView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {  // Denne metode skaber en ny instans,
+        // af vores embedde web browser og loader der efter bookingsystemets url.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
-        bookingView = (WebView) findViewById(R.id.bookingView);
+        bookingView = findViewById(R.id.bookingView);
         if (savedInstanceState != null) {
             bookingView.restoreState(savedInstanceState);
         } else {
-            bookingView.getSettings().setJavaScriptEnabled(true);
+            bookingView.getSettings().setJavaScriptEnabled(true); // Denne if else statement tjekker om hvorhvidt
+            // at savedInstanceState er falese. Hvis den ikker der det bliver der oprettede en række settings.
             bookingView.getSettings().setSupportZoom(true);
             bookingView.getSettings().setBuiltInZoomControls(false);
             bookingView.getSettings().setLoadWithOverviewMode(true);
@@ -35,8 +37,9 @@ public class BookingActivity extends AppCompatActivity {
             bookingView.setWebViewClient(new ourViewClient());
         }
         bookingView.loadUrl("https://system.easypractice.net/book/frederiksberg-sportsklinik#choose-service");
+        //  her loades vores booking system.
     }
-    public class ourViewClient extends WebViewClient {
+    public static class ourViewClient extends WebViewClient { // denne klasse står for cookie håndtering.
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
